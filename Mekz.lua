9a2d41e54865bfd4063efc9b619caab47f140d1b
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2022-12-12 21:54:49 +0700

    Update Mekz.lua

--- Mekz.lua
+++ Mekz.lua
@@ -1,3 +1,3 @@
 DataBuyer = {
-    {id = "1001607668772900864", Link = "https://raw.githubusercontent.com/ditampol-tzuyoon/iditem/main/Toked.lua"}
+    {id = "1001607668772900864", Link = "https://pastebin.com/raw/3DMFk00r"}
 }

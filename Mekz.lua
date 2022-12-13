89e4f92badf1ebae90c6a320a0bef9b65db0d5bc
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2022-12-13 12:30:35 +0700

    Update Mekz.lua

--- Mekz.lua
+++ Mekz.lua
@@ -3,10 +3,12 @@ DataBuyer = {
     {id = "982277409392955392", Link = "https://pastebin.com/raw/RBvEtwC1"},
     {id = "495584013625589762", Link = "https://pastebin.com/raw/vR1dnpNh"},
     {id = "550658413315751936", Link = "https://pastebin.com/raw/SENZk1jj"},
-    {id = "1052030106153005086", Link = "https://pastebin.com/raw/XPup0G0d"}
+    {id = "1052030106153005086", Link = "https://pastebin.com/raw/XPup0G0d"},
+    {id = "160210285594935296", Link = "https://pastebin.com/raw/M32bTFwc"}
 }
 
 DataBuyers = {
     {id = "1001607668772900864", Link = "https://pastebin.com/raw/3DMFk00r"},
-    {id = "982277409392955392", Link = "https://pastebin.com/raw/RBvEtwC1"}
+    {id = "982277409392955392", Link = "https://pastebin.com/raw/RBvEtwC1"},
+    {id = "160210285594935296", Link = "https://pastebin.com/raw/M32bTFwc"}
 }

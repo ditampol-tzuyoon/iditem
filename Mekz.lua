c7dbdde194c5045b64eda9de7e21b0af1e5ac92e
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2023-04-20 05:26:49 +0800

    Update Mekz.lua

--- Mekz.lua
+++ Mekz.lua
@@ -99,7 +99,8 @@ DataBuyer = {
     {id = "948410200199684106", Link = "https://pastebin.com/raw/wEQ7G3i6"},
     {id = "561514494283481110", Link = "https://pastebin.com/raw/SAuBnTbv"},
     {id = "487931555893870593", Link = "https://pastebin.com/raw/xiEzKfcD"},
-    {id = "853675179187109888", Link = "https://pastebin.com/raw/AWvZ73HD"}
+    {id = "853675179187109888", Link = "https://pastebin.com/raw/AWvZ73HD"},
+    {id = "454443422061821952", Link = "https://pastebin.com/raw/wvnjee1w"}
 }
 
 DataBuyers = {
@@ -154,5 +155,6 @@ DataBuyers = {
     {id = "816007324350021672", Link = "https://pastebin.com/raw/9cRRscQp"},
     {id = "513285129280552990", Link = "https://pastebin.com/raw/xxhi1eyH"},
     {id = "561514494283481110", Link = "https://pastebin.com/raw/SAuBnTbv"},
-    {id = "853675179187109888", Link = "https://pastebin.com/raw/AWvZ73HD"}
+    {id = "853675179187109888", Link = "https://pastebin.com/raw/AWvZ73HD"},
+    {id = "454443422061821952", Link = "https://pastebin.com/raw/wvnjee1w"}
 }
